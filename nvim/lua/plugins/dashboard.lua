b838a22800858db3528d745296cf221b942d28bd
Jufedev <jufesena@gmail.com>
2025-07-18 18:49:27 -0500

    Update dashboard.lua

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -21,7 +21,7 @@ return {
             [[                                                                                  ]],
           },
           center = {
-            { icon = "󰈞  ", desc = "Find File",    key = "f", action = "Telescope find_files" },
+            { icon = "󰈞  ", desc = "Find File",    key = "f", action = "Snacks.picker.files()" },
             { icon = "  ", desc = "New File",     key = "g", action = "ene" },
             { icon = "󱋡  ", desc = "Recent Files", key = "r", action = "Telescope oldfiles" },
             { icon = "  ", desc = "Config",       key = "c", action = "e ~/.config/nvim/init.lua" },
